b4efbee95a18318bc8c816b453e6add1a71b43dc
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-11 15:22:01 +0900

    switch-typeに関す記述

---
+++ 7-switch-type.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println(v * 2)
+	case string:
+		fmt.Println(v + "!")
+	default:
+		fmt.Println("I don't know %T\n", v)
+	}
+}
+
+func main() {
+	do(10)
+	do("Mike")
+	do(true)
+}
